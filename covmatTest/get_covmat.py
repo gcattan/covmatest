992d04a164d7655d449c9074e4a06f44f7b6eab6
gcattan <gregoire.cattan@ibm.com>
2021-10-06 20:27:28 +0200

    flake8

--- covmatTest/get_covmat.py
+++ covmatTest/get_covmat.py
@@ -25,13 +25,14 @@ trials = []
 def _get_trials(raw):
     events = mne.find_events(raw=raw, shortest_event=1, verbose=False)
     event_id = {'closed': 1, 'open': 2}
-    epochs = mne.Epochs(raw, events, event_id, tmin=2.0, tmax=8.0, baseline=None,
-                        verbose=False, preload=True)
+    epochs = mne.Epochs(raw, events, event_id, tmin=2.0, tmax=8.0,
+                        baseline=None, verbose=False, preload=True)
     epochs.pick_types(eeg=True)
     return epochs.get_data()
 
+
 def _init():
-    dataset = AlphaWaves(useMontagePosition = False) 
+    dataset = AlphaWaves(useMontagePosition=False)
 
     subjects = dataset.subject_list
     n_subjects = len(subjects)
@@ -43,14 +44,16 @@ def _init():
     trials = _get_trials(raw)
     return len(trials)
 
+
 def _get_covmat(n_channels):
     global trials
     n_trials = len(trials)
     if n_trials == 0:
         n_trials = _init()
     index = random.randint(0, n_trials - 1)
-    trial = np.array([trials[index][:n_channels,:]])
+    trial = np.array([trials[index][:n_channels, :]])
     return Covariances(estimator='lwf').fit_transform(trial)
+
 
 def get_covmat(n_matrices, n_channels):
     covset = np.zeros((n_matrices, n_channels, n_channels))
--- tests/test_get_covmat.py
+++ tests/test_get_covmat.py
@@ -4,23 +4,27 @@ from covmatTest import get_covmat
 def test_canary(is_spd, is_spsd):
     get_covmat(1, 1)
 
+
 def test_allowed_indices():
     for channel in range(1, 17):
         covmat = get_covmat(10, channel)
         assert covmat.shape == (10, channel, channel)
 
+
 def test_not_allowed_indices():
     for trial in [-1, 0]:
         for channel in [-1, 0, 17]:
             try:
-                covmat = get_covmat(trial, channel)
-                assert(False) # should not be possible
-            except:
+                get_covmat(trial, channel)
+                assert(False)  # should not be possible
+            except Exception:
                 pass
+
 
 def test_is_spd(is_spd):
     for channel in range(1, 17):
         assert(is_spd(get_covmat(100, channel)))
+
 
 def test_is_spsd(is_spsd):
     for channel in range(1, 17):
