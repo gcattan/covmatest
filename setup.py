5c1ca796fc333e04d39dc8bff7d74cfc9082c962
gcattan <gregoire.cattan@ibm.com>
2021-10-17 20:01:52 +0200

    update directory for flake8

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
 
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["alphawaves", "pyriemann"],
+    install_requires=["alphawaves"],
 
     platforms='any',
     
