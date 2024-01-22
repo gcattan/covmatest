0c0815c4f4cd8916d9bd1fdb7a5d282213fa7dad
gcattan <gcattan@hotmail.fr>
2024-01-22 22:47:55 +0100

    Update test_get_covmat.py
    fix instance not cleaned

--- tests/test_get_covmat.py
+++ tests/test_get_covmat.py
@@ -36,10 +36,8 @@ def test_seed():
 
 def test_returns_A_B():
     n_matrices, n_channels = 1, 1
-    classA = get_covmat(
-        n_matrices, n_channels, returns_A=True, returns_B=False, seed=42
-    )
-    classB = get_covmat(
-        n_matrices, n_channels, returns_A=False, returns_B=True, seed=42
-    )
+    classA = CovmatGen(returns_A=True, returns_B=False)
+        .get_covmat(n_matrices, n_channels)
+    classB = CovmatGen(returns_A=False, returns_B=True)
+        .get_covmat(n_matrices, n_channels)
     assert not classA[0][0] == classB[0][0]
